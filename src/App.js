dae8b705fac93f163b4b904e5f1d94b11f03714c
Ralpis <rotjsdn1@naver.com>
2020-10-30 15:49:02 +0900

    수정함:        package.json 	수정함:        src/App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
-const foodILike = [
-  {
-    id:1,
-    name:"Kimchi",
-    image:"https://search.pstatic.net/common/?src=http%3A%2F%2Fcafefiles.naver.net%2F20150523_34%2Fsychoo99_1432385796335S6vvD_JPEG%2FNa1432385629377.jpg",
-    rating:5
-  },
-  {
-    id:2,
-    name:"Burger",
-    image:"https://search.pstatic.net/sunny/?src=https%3A%2F%2Fmedia.timeout.com%2Fimages%2F104098083%2F630%2F472%2Fimage.jpg&type=b400",
-    rating: 5
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("Hello")
   }
 
+  state = {
+    count: 0
+  };
 
-];
+  add = () =>{
+    this.setState(current =>({
+      count: current.count + 1
+    }))
+  };
+  minus = () =>{
+    this.setState(current =>({
+      count: current.count - 1
+    }))
+  };
+  componentDidMount(){
+    console.log("j")
+  }
+  componentDidUpdate(){
+    console.log("update")
+  }
 
-
-function Food({ name, image }){
-  return <div>
-    <h1>I Like {name}.</h1>
-    <img src={image} alt ={name}/>
-  </div>;
+  render(){ 
+    return <div>
+      <h2>The number is: { this.state.count }</h2>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>;
+  }
 }
 
-
-function App() {
-  return <div>
-    <h1>Hello</h1>
-  {foodILike.map(dish => 
-  (<Food key = {dish.id} name = {dish.name} image = {dish.image}></Food>))}
-
-  </div>;
-}
 
 export default App;
